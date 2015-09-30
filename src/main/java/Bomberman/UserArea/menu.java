507242e5448e33cfa79a248e82bd5301a6d32da1
micha <michswo@web.de>
2015-09-30 16:56:28 +0200

    s

--- src/main/java/Bomberman/UserArea/menu.java
+++ src/main/java/Bomberman/UserArea/menu.java
@@ -18,11 +18,6 @@ public class menu extends Zone {
     public menu(UserArea parent, int i, int i1, int i2, int i3) {
         super(i, i1, i2, i3);
         this.parent = parent;
-
-        options = new ContainerZone("Options",getWidth(),getHeight(),getWidth(),getHeight());
-        options.add(new ButtonZone("PlayButton", getWidth(), getHeight(), 100, 50, "mitspielen", 10));
-        options.add(new ButtonZone("OptionsButton", getWidth(), getHeight()+50, 100, 50, "Aufgeben", 10));
-        options.add(new ButtonZone("ExitButton",getWidth(),getHeight()+100,100,50,"Spiel beenden",10));
     }
     @Override
     public void draw() {
@@ -33,8 +28,18 @@ public class menu extends Zone {
         textAlign(CENTER);  textSize(10);   fill(0);
         text("menu", 0, 0);
     }
+    public void drawMenu() {
+        stroke(0, 0, 0);//legt Randfarbe nachfolgender Formen fest
+        fill(192, 50, 0);//legt Füllfarbe nachfolgender Formen fest
+        rect( 0, -55, this.getHeight(), this.getHeight());
+        textAlign(CENTER);  textSize(10);   fill(0);
+        text("Neu", 0, -55);
+
+    }
 
     //touch method
+    @Override
+    public void touch() {drawMenu(); }
     @Override
     public void touchDown(Touch touch){} //nur der Moment des Touches
     @Override
