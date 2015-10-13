98001f697705244b7366996c7e5003c037b60fe0
micha <michswo@web.de>
2015-10-13 17:28:37 +0200

    Popup Menü + Dialog Feld für Aufgeben/Beenden/neu

--- src/main/java/Bomberman/UserArea/menu.java
+++ src/main/java/Bomberman/UserArea/menu.java
@@ -2,11 +2,6 @@ package Bomberman.UserArea;
 
 import vialab.SMT.Touch;
 import vialab.SMT.Zone;
-import vialab.SMT.*;
-
-/**
- * Created by tobias on 15.07.15.
- */
 
 public class menu extends Zone {
 
@@ -17,47 +12,70 @@ public class menu extends Zone {
         super(i, i1, i2, i3);
         this.parent = parent;
 
-
-        //SMT.add(new LeftPopUpMenuZone(300, 100, 50, 50, 200, 100, "Aufgeben", "Beenden", "zur�ck"));
-        // http://vialab.science.uoit.ca/smt/reference/LeftPopUpMenuZone/LeftPopUpMenuZone.php
-        // http://vialab.science.uoit.ca/smt/examples/Zones/LeftPopUpMenu.php
     }
     @Override
     public void draw() {
         stroke(0, 0, 0);//legt Randfarbe nachfolgender Formen fest
         fill(192, 0, 0);//legt F�llfarbe nachfolgender Formen fest
         rectMode(CORNER);
-        rect(-16, -20, this.getHeight(), this.getHeight());
-        textAlign(CENTER);  textSize(10);   fill(0);
-        text("menu", 0, 0);
+        rect(-16, -16, this.getHeight()*5/2, this.getHeight()-3);
+        textAlign(CENTER);  textSize(15);   fill(0);
+        text("Menu", 25, 3);
         if (pop) {
             drawMenu();
+            drawDialog();
         }
     }
     public void drawMenu() {
-        // Menu
-        stroke(0, 0, 0);//legt Randfarbe nachfolgender Formen fest
-        fill(192, 50, 0);//legt F�llfarbe nachfolgender Formen fest
+        // Neu
+        stroke(0, 0, 0);
+        fill(192, 50, 0);
         rectMode(CORNER);
-        rect(-16, -55, this.getHeight(), this.getHeight());
-        textAlign(CENTER);  textSize(10);   fill(0);
-        text("Neu", 0, -35);
+        rect(-16, -48, this.getHeight()*5/2, this.getHeight()-3);
+        textAlign(CENTER);  textSize(15);   fill(0);
+        text("Neu", 25, -27);
 
         //Beenden
-        stroke(0, 0, 0);//legt Randfarbe nachfolgender Formen fest
-        fill(192, 50, 0);//legt F�llfarbe nachfolgender Formen fest
+        stroke(0, 0, 0);
+        fill(192, 50, 0);
         rectMode(CORNER);
-        rect(-16, -90, this.getHeight(), this.getHeight());
-        textAlign(CENTER);  textSize(10);   fill(0);
-        text("close", 0, -70);
+        rect(-16, -80, this.getHeight()*5/2, this.getHeight()-3);
+        textAlign(CENTER);  textSize(15);   fill(0);
+        text("Beenden", 25, -61);
 
         // Aufgeben
-        stroke(0, 0, 0);//legt Randfarbe nachfolgender Formen fest
-        fill(192, 50, 0);//legt F�llfarbe nachfolgender Formen fest
+        stroke(0, 0, 0);
+        fill(192, 50, 0);
         rectMode(CORNER);
-        rect(-16, -125, this.getHeight(), this.getHeight());
-        textAlign(CENTER);  textSize(10);   fill(0);
-        text("aufgeben", 0, -105);
+        rect(-16, -113, this.getHeight()*5/2, this.getHeight()-3);
+        textAlign(CENTER);  textSize(15);   fill(0);
+        text("Aufgeben", 25, -95);
+    }
+    public void drawDialog() {
+
+        // Dialog Feld
+        stroke(0, 0, 0);
+        fill(200, 100, 50);
+        rectMode(CORNER);
+        rect(this.getWidth() * 3, -this.getHeight() * 5 / 2 - 5, this.getWidth() * 6, this.getHeight() * 3);
+        textAlign(CENTER);  textSize(15);   fill(0);
+        text("Willst du wirklich \n Aufgeben?", this.getWidth() * 6, -this.getHeight() * 2);
+
+        // Ja
+        stroke(0, 0, 0);
+        fill(192, 150, 0);
+        rectMode(CORNER);
+        rect(this.getWidth() * 3, -this.getHeight() + 12, this.getWidth() * 2, this.getHeight());
+        textAlign(CENTER);  textSize(15);   fill(0);
+        text("Ja", this.getWidth()*4, -this.getHeight()*1/8);
+
+        // Nein
+        stroke(0, 0, 0);
+        fill(191, 150, 0);
+        rectMode(CORNER);
+        rect(this.getWidth() * 7, -this.getHeight() + 12, this.getWidth() * 2, this.getHeight());
+        textAlign(CENTER);  textSize(15);   fill(0);
+        text("Nein", this.getWidth()*8, -this.getHeight()*1/8);
     }
 
 
