19465fd9b5dc7f61b2c66aa6d61ea7e2e662fec4
micha <michswo@web.de>
2015-10-22 22:17:47 +0200

    Menübuttons als einzelne Klasse angelegt

--- src/main/java/Bomberman/UserArea/menu.java
+++ src/main/java/Bomberman/UserArea/menu.java
@@ -1,5 +1,8 @@
 package Bomberman.UserArea;
 
+import Bomberman.UserArea.MenuButtons.ButtonClose;
+import Bomberman.UserArea.MenuButtons.ButtonNew;
+import Bomberman.UserArea.MenuButtons.ButtonSurr;
 import vialab.SMT.Touch;
 import vialab.SMT.Zone;
 
@@ -7,77 +10,31 @@ public class menu extends Zone {
 
     private UserArea parent;
     private boolean pop = false;
+    private ButtonClose button_close;
+    private ButtonNew button_new;
+    private ButtonSurr button_surr;
 
     public menu(UserArea parent, int i, int i1, int i2, int i3) {
         super(i, i1, i2, i3);
         this.parent = parent;
-
+        button_close = new ButtonClose(parent, 0,this.getHeight()/2- this.getHeight()/4-2,this.getWidth(),this.getHeight());
+        button_new = new ButtonNew(parent, 0,this.getHeight()- this.getHeight()/2,this.getWidth(),this.getHeight());
+        button_surr = new ButtonSurr(parent, 0,this.getHeight()- this.getHeight()/2 -(this.getHeight() / 4)*3 -this.getHeight()/16,this.getWidth(),this.getHeight());
     }
     @Override
     public void draw() {
-        stroke(0, 0, 0);//legt Randfarbe nachfolgender Formen fest
-        fill(192, 0, 0);//legt F�llfarbe nachfolgender Formen fest
+        stroke(0, 0, 0);
+        fill(192, 0, 0);
         rectMode(CORNER);
-        rect(-16, -16, this.getHeight()*5/2, this.getHeight()-3);
+        rect(0, 0, this.getWidth() / 2 + this.getWidth() / 6, this.getHeight() / 4);
         textAlign(CENTER);  textSize(15);   fill(0);
-        text("Menu", 25, 3);
+        text("Menu", this.getWidth()/3, this.getHeight()/6);
         if (pop) {
-            drawMenu();
-            drawDialog();
+           this.add(button_close);
+            this.add(button_new);
+            this.add(button_surr);
         }
     }
-    public void drawMenu() {
-        // Neu
-        stroke(0, 0, 0);
-        fill(192, 50, 0);
-        rectMode(CORNER);
-        rect(-16, -48, this.getHeight()*5/2, this.getHeight()-3);
-        textAlign(CENTER);  textSize(15);   fill(0);
-        text("Neu", 25, -27);
-
-        //Beenden
-        stroke(0, 0, 0);
-        fill(192, 50, 0);
-        rectMode(CORNER);
-        rect(-16, -80, this.getHeight()*5/2, this.getHeight()-3);
-        textAlign(CENTER);  textSize(15);   fill(0);
-        text("Beenden", 25, -61);
-
-        // Aufgeben
-        stroke(0, 0, 0);
-        fill(192, 50, 0);
-        rectMode(CORNER);
-        rect(-16, -113, this.getHeight()*5/2, this.getHeight()-3);
-        textAlign(CENTER);  textSize(15);   fill(0);
-        text("Aufgeben", 25, -95);
-    }
-    public void drawDialog() {
-
-        // Dialog Feld
-        stroke(0, 0, 0);
-        fill(200, 100, 50);
-        rectMode(CORNER);
-        rect(this.getWidth() * 3, -this.getHeight() * 5 / 2 - 5, this.getWidth() * 6, this.getHeight() * 3);
-        textAlign(CENTER);  textSize(15);   fill(0);
-        text("Willst du wirklich \n Aufgeben?", this.getWidth() * 6, -this.getHeight() * 2);
-
-        // Ja
-        stroke(0, 0, 0);
-        fill(192, 150, 0);
-        rectMode(CORNER);
-        rect(this.getWidth() * 3, -this.getHeight() + 12, this.getWidth() * 2, this.getHeight());
-        textAlign(CENTER);  textSize(15);   fill(0);
-        text("Ja", this.getWidth()*4, -this.getHeight()*1/8);
-
-        // Nein
-        stroke(0, 0, 0);
-        fill(191, 150, 0);
-        rectMode(CORNER);
-        rect(this.getWidth() * 7, -this.getHeight() + 12, this.getWidth() * 2, this.getHeight());
-        textAlign(CENTER);  textSize(15);   fill(0);
-        text("Nein", this.getWidth()*8, -this.getHeight()*1/8);
-    }
-
 
     //touch method
     @Override
